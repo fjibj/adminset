989e5b327069a262d9e8cf7a4fd30b4b86c79eb1
方进 <fjibj@hotmail.com>
2017-11-25 12:39:27 +0800

    修改cpu数量获取方法，如果取不到就设置为0

--- install/client/adminset_agent.py
+++ install/client/adminset_agent.py
@@ -11,7 +11,7 @@ import json
 import threading
 
 token = 'HPcWR7l4NJNJ'
-server_ip = '192.168.47.130'
+server_ip = '10.45.59.248'
 
 try:
     import psutil
@@ -91,7 +91,7 @@ def get_cpu_model():
 
 
 def get_cpu_cores():
-    cpu_cores = {"logical": psutil.cpu_count(logical=False), "physical": psutil.cpu_count()}
+    cpu_cores = {"physical": psutil.cpu_count(logical=False) if psutil.cpu_count(logical=False) else 0, "logical": psutil.cpu_count()}
     return cpu_cores
 
 
